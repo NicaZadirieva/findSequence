fb5339f1d03cabe68e8acbd6c0a273b565140dca
Veronica <no.demchenko@yandex.ru>
2025-02-27 15:49:14 +0300

    feat(util): добавила поиск центра в массиве
    центр - ***

---
+++ util.js
@@ -0,0 +1,21 @@
+function getCenterElementInArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (i + 9 > array.length) {
+      return;
+    }
+    const formedWord = array.slice(i, i + 9);
+    console.log("formedWord", formedWord);
+    const hasXXX =
+      formedWord[0] == formedWord[1] && formedWord[1] == formedWord[2];
+    const hasYYY =
+      formedWord[6] == formedWord[7] && formedWord[6] == formedWord[8];
+
+    if (hasXXX && hasYYY) {
+      console.log("result", formedWord.slice(3, 6));
+    }
+  }
+}
+
+const array = [0, 0, 0, 1, 1, 1, 2, 2, 2, 1, 1, 1];
+
+getCenterElementInArray(array);
